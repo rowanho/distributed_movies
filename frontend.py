a78107a352850f4debe4e32f2cdeaea82065779f
Rowan <rowanho45@gmail.com>
2019-02-09 15:29:50 +0000

    Basic class framework

--- frontend.py
+++ frontend.py
@@ -1,15 +1,33 @@
 # saved as greeting-server.py
 import Pyro4
 @Pyro4.expose
-class GreetingMaker(object):
-    def get_fortune(self, name):
-        return "Hello, {0}. Here is your fortune message:\n" \
-               "Tomorrow's lucky number is 12345678.".format(name)
+class Movie_Updates(object):
+    def register_user(self):
+        #get a replica implementation of the function
+        movie_updates = Pyro4.Proxy("PYRONAME:replica.movie.updates")
+        return movie_updates.register_user()
+    def add_rating(self,movie_name,user_id):
+        #get a replica implementation of the function
+        movie_updates = Pyro4.Proxy("PYRONAME:replica.movie.updates")
+        return movie_updates.add_rating()
+@Pyro4.expose
+class Movie_Queries(object):
+    def get_overall_rating(self,movie_name):
+        movie_queries = Pyro4.Proxy("PYRONAME:replica.movie.queries")
+        #list of tuples containing (movie_id,movie_name)
+        possible_movies = movie_queries.get_movies(movie_name)
+        rating = movie_queries.get_overall_rating(possible_movies[0][0])
+        return rating
 
-daemon = Pyro4.Daemon()                # make a Pyro daemon
-ns = Pyro4.locateNS()                  # find the name server
-uri = daemon.register(GreetingMaker)   # register the greeting maker as a Pyro object
-ns.register("example.greeting", uri)   # register the object with a name in the name server
-
-print("Ready.")
-daemon.requestLoop()                   # start the event loop of the server to wait for calls
+#register all the classes here
+def main():
+    daemon = Pyro4.Daemon()                # make a Pyro daemon
+    ns = Pyro4.locateNS()                  # find the name server
+    uri = daemon.register(Movie_Updates)   # register the greeting maker as a Pyro object
+    ns.register("frontend.movie.updates", uri)   # register the object with a name in the name server
+    uri2 = daemon.register(Movie_Queries)
+    ns.register("frontend.movie.queries",uri2)
+    print("Ready.")
+    daemon.requestLoop()                   # start the event loop of the server to wait for calls
+if __name__ == "__main__":
+    main()
--- replica.py
+++ replica.py
@@ -1,2 +1,35 @@
 import Pyro4
 import csv
+
+#TODO - complete functions and classes!
+@Pyro4.expose
+class Movie_Updates(object):
+    def register_user(self):
+        return 0
+    #updates the movie rating based on a new rating
+    def update_rating(self, user_id,movie_id,rating):
+        return 0
+@Pyro4.expose
+class Movie_Queries(object):
+    #returns a list of tuples containing movie_ids and full names based on movie name input
+    def get_movies(self,movie_string):
+        return [(0,'')]
+    #gets the rating of a movie by user_id and movie_id
+    def get_user_rating(self,user_id,movie_id):
+        return 0
+    #returns the rating of a movie by all users
+    def get_overall_rating(self,movie_id):
+        return 0
+#register all the classes here
+def main():
+    daemon = Pyro4.Daemon()                # make a Pyro daemon
+    ns = Pyro4.locateNS()                  # find the name server
+    uri = daemon.register(Movie_Updates)   # register the greeting maker as a Pyro object
+    ns.register("replica.movie.updates", uri)   # register the object with a name in the name server
+    uri2 = daemon.register(Movie_Queries)
+    ns.register("replica.movie.queries",uri2)
+    print("Ready.")
+    daemon.requestLoop()                   # start the event loop of the server to wait for calls
+
+if __name__ == "__main__":
+    main()
