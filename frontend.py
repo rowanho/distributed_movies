bf6bc2147217e5628557d42dec18e0e85285f1be
Rowan <rowanho45@gmail.com>
2019-02-11 21:11:22 +0000

    Changed to single class implementation

--- frontend.py
+++ frontend.py
@@ -1,32 +1,47 @@
 # saved as greeting-server.py
 import Pyro4
 import threading
+
+class NoFreeServerException(Exception):
+    pass
 @Pyro4.expose
-class Movie_Updates(object):
+class FrontEnd(object):
     def register_user(self):
         #get a replica implementation of the function
-        movie_updates = Pyro4.Proxy("PYRONAME:replica.movie.updates")
-        return movie_updates.register_user()
+        replica_id = get_free_server()
+        if replica_id != 0:
+            replica = Pyro4.Proxy("PYRONAME:" + replica_id +".replica")
+            return replica.register_user()
+        else:
+            raise NoFreeServerException("Couldn't find a free server")
     def add_rating(self,movie_name,user_id):
-        #get a replica implementation of the function
-        movie_updates = Pyro4.Proxy("PYRONAME:replica.movie.updates")
-        return movie_updates.add_rating()
-@Pyro4.expose
-class Movie_Queries(object):
+        replica_id = get_free_server()
+        if replica_id != 0:
+            #get a replica implementation of the function
+            replica = Pyro4.Proxy("PYRONAME:replica")
+            return movie_updates.add_rating()
+        else:
+            raise NoFreeServerException("Couldn't find a free server")
+
     def get_overall_rating(self,movie_name):
-        movie_queries = Pyro4.Proxy("PYRONAME:replica.movie.queries")
-        #list of tuples containing (movie_id,movie_name)
-        possible_movies = movie_queries.get_movies(movie_name)
-        print(possible_movies[0][0])
-        rating = movie_queries.get_overall_rating(possible_movies[0][0])
-        return possible_movies[0][1],rating
+        replica_id = get_free_server()
+        if replica_id != 0:
+            replica = Pyro4.Proxy("PYRONAME:" + replica_id +".replica")
+            #list of tuples containing (movie_id,movie_name)
+            possible_movies = replica.get_movies(movie_name)
+            print(possible_movies[0][0])
+            rating = replica.get_overall_rating(possible_movies[0][0])
+            return possible_movies[0][1],rating
+        else:
+            raise NoFreeServerException("Couldn't find a free server")
 
 #input - the Pyro4 ns object
 #iterates through servers on the name system and returns the uuid of the one that is free
 #if it can't find one, returns 0
-def get_free_server(ns):
+def get_free_server():
+    ns = Pyro4.locateNS()
     for key in ns.list():
-        if 'replica.status' in key:
+        if 'replica' in key:
             replica_status = Pyro4.Proxy("PYRONAME:" + key)
             is_free = False
             try:
@@ -45,16 +60,13 @@ def get_free_server(ns):
 def main():
     daemon = Pyro4.Daemon()                # make a Pyro daemon
     ns = Pyro4.locateNS()                  # find the name server
-    uri = daemon.register(Movie_Updates)   # register the greeting maker as a Pyro object
-    ns.register("frontend.movie.updates", uri)   # register the object with a name in the name server
-    uri2 = daemon.register(Movie_Queries)
-    ns.register("frontend.movie.queries",uri2)
+    uri = daemon.register(FrontEnd)   # register the greeting maker as a Pyro object
+    ns.register("frontend", uri)   # register the object with a name in the name server
     print("Ready.")
     def my_loop():
         threading.Timer(3.0, my_loop).start()
-        print("First free server found: ",get_free_server(ns))
+        print("First free server found: ",get_free_server())
     my_loop()
-
     daemon.requestLoop()                   # start the event loop of the server to wait for calls
 if __name__ == "__main__":
     main()
--- replica.py
+++ replica.py
@@ -4,34 +4,28 @@ from uuid import uuid1
 import threading
 MOVIE_DIR = 'ml-latest-small'
 #STATUS is either set to 'free','busy' or 'down'
-STATUS = 'free'
 #TODO - complete functions and classes!
 #raise this error when we can't find a movie
 class NullQueryException(Exception):
     pass
 #remote functions to get the status of the server
 @Pyro4.expose
-class Replica_Status(object):
+class Replica(object):
+    def __init__(self):
+        self.status = 'free'
     #gets the status of the server
     def get_status(self):
-        return STATUS
-#remote functions for sending and recieving gossip
-@Pyro4.expose
-class Gossip(object):
+        return self.status
+
     def recieve_gossip(self,gossip):
         pass
-#remote functions for sending updates for movie ratings
-@Pyro4.expose
-class Movie_Updates(object):
+
     def register_user(self):
         return 0
     #updates the movie rating based on a new rating
     def update_rating(self, user_id,movie_id,rating):
         return 0
 
-#remote functions for obtaining queries for movie ratings
-@Pyro4.expose
-class Movie_Queries(object):
     #returns a list of tuples containing movie_ids and full names based on movie name input
     def get_movies(self,movie_string):
         movie_list = []
@@ -46,6 +40,7 @@ class Movie_Queries(object):
             raise NullQueryException("No movies found")
         else:
             return movie_list
+
     #gets the rating of a movie by user_id and movie_id
     def get_user_rating(self,user_id,movie_id):
         rating = -1.0
@@ -60,6 +55,7 @@ class Movie_Queries(object):
             raise NullQueryException('No ratings found')
         else:
             return rating
+
     #returns the rating of a movie by all users
     def get_overall_rating(self,movie_id):
         r_count,total_rating = 0,0
@@ -86,12 +82,8 @@ def main():
     id_string = str(uuid1())
     daemon = Pyro4.Daemon()                # make a Pyro daemon
     ns = Pyro4.locateNS()                  # find the name server
-    uri = daemon.register(Movie_Updates)   # register the greeting maker as a Pyro object
-    ns.register(id_string + ".movie.updates", uri)   # register the object with a name in the name server
-    uri2 = daemon.register(Movie_Queries)
-    ns.register(id_string + ".movie.queries",uri2)
-    uri3 = daemon.register(Replica_Status)
-    ns.register(id_string + ".replica.status",uri3)
+    uri = daemon.register(Replica)   # register the greeting maker as a Pyro object
+    ns.register(id_string + ".replica", uri)   # register the object with a name in the name server
     print("Server with id %s is ready." %(id_string))
     daemon.requestLoop()                   # start the event loop of the server to wait for calls
 
